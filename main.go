7746566397941745fd0901cabe8c9ebceac95a77
derichard <16598223+derichard@users.noreply.github.com>
2025-02-05 21:24:14 +0100

    fix format

--- main.go
+++ main.go
@@ -16,11 +16,11 @@ import (
 
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
-func unused() {
-    // this function does nothing
-    // and is called nowhere
-}
 
+func unused() {
+	// this function does nothing
+	// and is called nowhere
+}
 
 type apiConfig struct {
 	DB *database.Queries
